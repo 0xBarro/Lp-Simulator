75e478f90ccf3632b43d2011b7c87f90581808c4
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-30 17:37:23 +0100

    Finished the swap mechanic

--- lp.js
+++ lp.js
@@ -1,6 +1,5 @@
 let chart = undefined;
 
-
 const createChart = () => {
 
     values = getValues()
@@ -27,10 +26,7 @@ const createChart = () => {
         options: {}
       };
     
-        chart = new Chart(
-      document.getElementById('myChart'),
-      config
-    );
+      chart = new Chart(document.getElementById('myChart'), config );
 }
 
 
@@ -57,3 +53,50 @@ const getValues = () => {
   chart.data.datasets[0].data = formValues.data
   chart.update()
 }
+
+const swapConstant = () => {
+  const amountSold = +document.getElementById("amount").value
+  const tA = document.getElementsByName("ts")[0].checked
+  const tB = document.getElementsByName("ts")[1].checked
+
+  console.log(tA, tB)
+
+  if (!tA && !tB) {
+    alert("Select one token to swap!")
+  } else if (amountSold <= 0) {
+    alert("Amount to swap must be geater than 0!")
+  } else {
+    const k =  +document.getElementById("assetANT").value * +document.getElementById("assetBNT").value  // Pool constant
+
+    console.log("Constant: ", k)
+
+    // TODO: Improve this
+    const tokenSold = (tA) ? "assetANT" : "assetBNT"
+    const tokenBought = (tA) ? "assetBNT" : "assetANT"
+
+    // Add token to the pool
+    document.getElementById(tokenSold).value = +document.getElementById(tokenSold).value + amountSold;
+
+    // Calculate the number obtained of the other asset. 
+    // The constant formula looks like this: nTokensA * nTokensB = k.
+
+    const amountBought = k / +document.getElementById(tokenSold).value
+    document.getElementById(tokenBought).value = +document.getElementById(tokenBought).value - amountBought;
+
+    console.log(amountBought)
+
+    updateChart()
+
+  }
+}
+
+const swap = () => {
+  poolModes[poolMode]()
+}
+
+
+const poolModes = {
+  constant: swapConstant
+}
+
+let poolMode = "constant"
