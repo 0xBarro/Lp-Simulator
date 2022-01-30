46e2e914a61176ddfcca7464c60d5a92c170498b
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-30 23:26:25 +0100

    Correct pool calcution after swap

--- lp.js
+++ lp.js
@@ -1,102 +1,104 @@
-let chart = undefined;
-
+// Initial funtion to create the chart
 const createChart = () => {
 
-    values = getValues()
-    console.log(values, values.data)
+  values = getValues()
+  console.log(values, values.data)
 
-    const labels = [
-        'Asset A',
-        'Asset B',
-      ];
-    
-      const data = {
-        labels: labels,
-        datasets: [{
-          label: 'LP Token Balance',
-          backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
-          borderColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
-          data: values.data,
-        }]
-      };
-    
-      const config = {
-        type: 'doughnut',
-        data: data,
-        options: {}
-      };
-    
-      chart = new Chart(document.getElementById('myChart'), config );
+  const labels = [
+    'Asset A',
+    'Asset B',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'LP Token Balance',
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
+      borderColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
+      data: values.data,
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {}
+  };
+
+  chart = new Chart(document.getElementById('myChart'), config);
 }
 
-
-const balancePool = () => {
-  const a = 1;
-}
-
+// Get's the form values 
 const getValues = () => {
   return {
     nA: +document.getElementById("assetANT").value,
     upA: +document.getElementById("assetAUP").value,
     nB: +document.getElementById("assetBNT").value,
     upB: +document.getElementById("assetBUP").value,
-    get vA() {return this.nA * this.upA},
-    get vB() {return this.nB * this.upB},
-    get data() {return [this.vA, this.vB]}
+    get vA() { return this.nA * this.upA },
+    get vB() { return this.nB * this.upB },
+    get data() { return [this.vA, this.vB] }
   }
 }
 
- const updateChart = () => {
-  const formValues = getValues()
+// Updates the chards based on the form values
+const updateChart = () => {
 
   // Update Charts
-  chart.data.datasets[0].data = formValues.data
+  chart.data.datasets[0].data = getValues().data
   chart.update()
 }
 
-const swapConstant = () => {
-  const amountSold = +document.getElementById("amount").value
-  const tA = document.getElementsByName("ts")[0].checked
-  const tB = document.getElementsByName("ts")[1].checked
-
-  console.log(tA, tB)
-
-  if (!tA && !tB) {
-    alert("Select one token to swap!")
-  } else if (amountSold <= 0) {
-    alert("Amount to swap must be geater than 0!")
-  } else {
-    const k =  +document.getElementById("assetANT").value * +document.getElementById("assetBNT").value  // Pool constant
-
-    console.log("Constant: ", k)
-
-    // TODO: Improve this
-    const tokenSold = (tA) ? "assetANT" : "assetBNT"
-    const tokenBought = (tA) ? "assetBNT" : "assetANT"
-
-    // Add token to the pool
-    document.getElementById(tokenSold).value = +document.getElementById(tokenSold).value + amountSold;
-
-    // Calculate the number obtained of the other asset. 
-    // The constant formula looks like this: nTokensA * nTokensB = k.
-
-    const amountBought = k / +document.getElementById(tokenSold).value
-    document.getElementById(tokenBought).value = +document.getElementById(tokenBought).value - amountBought;
-
-    console.log(amountBought)
-
-    updateChart()
-
-  }
+const swapConst = (nA, nB, sellAmountA) => {
+  const k = nA * nB
+  const tokensReturned =  nB - (k/(nA + sellAmountA))
+  console.log(nA, nB, sellAmountA, k, tokensReturned)
+  return tokensReturned
 }
 
+// Adjusts the pool based on the selected swap mode
 const swap = () => {
-  poolModes[poolMode]()
+
+  const buyTokenPoolAmount = +document.getElementById(buyToken + "NT").value
+  const sellTokenPoolAmount = +document.getElementById(sellToken + "NT").value
+  const sellAmount = +document.getElementById("sellAmount").value
+
+  const boughtAmount = poolModes[poolMode](sellTokenPoolAmount, buyTokenPoolAmount, sellAmount)
+
+  // Update quantities in the pool
+  document.getElementById(buyToken + "NT").value = buyTokenPoolAmount - boughtAmount
+  document.getElementById(sellToken + "NT").value = sellTokenPoolAmount + sellAmount
+
+  // Update the chart
+  updateChart()
 }
 
+const invsereSwap = () => {
+  buyToken, sellToken = sellToken, buyToken
+  
+  // Update the tags
+  fillTokenNames()
+}
+
+const fillTokenNames = () => {
+  document.getElementById("sellToken").innerText = sellToken
+  document.getElementById("buyToken").innerText = buyToken
+}
+
+// Called when starting the page
+const init = () => {
+  createChart() // Draw the plot
+  fillTokenNames()
+}
+
+
+// Some global variables
+let chart = undefined;
 
 const poolModes = {
-  constant: swapConstant
+  constant: swapConst
 }
 
 let poolMode = "constant"
+let buyToken = "assetA"
+let sellToken  = "assetB"
